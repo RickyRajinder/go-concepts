a7ea949fff4a9b818435e7a9e579be4485016d33
rickyrajinder <singh.sangh@gmail.com>
2019-05-22 20:33:12 -0700

    Experiment with flow control

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Printf("hello, world\n")
+	scanner := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter Text: ")
+		text, _ := scanner.ReadString('\n')
+		if len(text) > 0 {
+			fmt.Println(text)
+		}
+	}
 }
